5bf30583fd99e1c6c9d0835d748f9ae61e78bc87
DresssOrtiz <134727739+DresssOrtiz@users.noreply.github.com>
2025-03-27 10:37:51 -0500

    Update Nodo.h
    Hice el nodo.h

--- Nodo.h
+++ Nodo.h
@@ -1,1 +1,19 @@
+#ifndef NODO_H
+#define NODO_H
+
+struct Nodo {
+    int dato;
+    int altura;
+    Nodo* izquierdo;
+    Nodo* derecho;
+
+    Nodo(int valor) {
+        dato = valor;
+        altura = 1; // nuevo nodo siempre se crea con altura 1
+        izquierdo = nullptr;
+        derecho = nullptr;
+    }
+};
+
+#endif
 
